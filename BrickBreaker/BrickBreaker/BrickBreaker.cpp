b17efb009044d73c710a2c2917214330bdfcc667
Conakry Coggins <conakryc@gmail.com>
2016-11-21 12:00:57 -0800

    Initial brick class implementation
    Basic variables and methods for the Brick object class

--- BrickBreaker/BrickBreaker/BrickBreaker.cpp
+++ BrickBreaker/BrickBreaker/BrickBreaker.cpp
@@ -15,7 +15,17 @@ private:
 };
 
 class Brick {
-
+public:
+	Brick() { x = y = 0.0f; width = height = 0.0f; }
+	Brick(float xNew, float yNew, float w, float h) { x = xNew; y = yNew; width = w; height = h; }
+	void setState(GLboolean);
+	void draw();
+	void setLocation(GLfloat, GLfloat);
+	void setSize(GLfloat, GLfloat);
+private: 
+	GLboolean state;
+	GLfloat width, height;
+	GLfloat x, y;
 };
 
 class Ball {
@@ -26,6 +36,24 @@ class Paddle {
 
 };
 
-boolean collisionCheck{
+void Brick::setLocation(GLfloat xNew, GLfloat yNew) {
+	x = xNew;
+	y = yNew;
+}
+
+void Brick::setSize(GLfloat w, GLfloat h) {
+	width = w;
+	height = h;
+}
+
+void Brick::setState(GLboolean s) {
+	state = s;
+}
+
+void Brick::draw() {
+	glRectf(x, y, x + width, y + height);
+}
+
+GLboolean collisionCheck() {
 
 }
